7f57cdf1aca0dc90fda7269cd99fe6e8b7366fbc
Prakash A <prakashaathi321@gmail.com>
2023-08-28 19:02:34 +0530

    dynamic card reusable 7.02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import Card from './components/Card';
+import "./App.css";
+import Card from "./components/Card";
+import pic from "./images/profile.jpeg"
 
 function App() {
   return (
     <div>
-       <Card></Card>
+      <Card
+        pic={pic}
+        name="Prakash A"
+        role="Software Developer"
+        desc="Full Stack Developer with a passion for developing scalable web applications.
+              Focused on efficient learning and eager to tackle more complex problems."
+        tech="React js, tailwind css, sass, Next js,
+        Spring boot, Flask"
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import "../components/Card.css"
 
-const Card = () => {
+const Card = ({pic,name,role,desc,tech}) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
     const handleCardClick = () => {
@@ -20,20 +20,17 @@ const Card = () => {
                 <div className="card__face card__face--back">
                     <div className="card__content">
                         <div className="card__header">
-                            <img src="https://avatars.githubusercontent.com/u/76159067?v=4" alt="profilePic" className="pp" />
-                            <h2>Prakash A</h2>
+                            <img src={pic} alt="profilePic" className="pp" />
+                            <h2>{ name }</h2>
                         </div>
                         <div className="card__body">
-                            <h3>Software developer</h3>
+                            <h3>{ role }</h3>
                             <p>
-                                Full Stack Developer with a passion for developing scalable web applications.
-                                Focused on efficient learning and eager to tackle more complex problems.
+                                {desc}
                                 <br /> <br />
                                 <strong>Tech Stack:</strong>
                                 <br />
-                                <i>React js, tailwind css, sass, Next js</i>
-                                <br />
-                                <i>Spring boot, Flask</i>
+                                <i>{ tech }</i>
                             </p>
                         </div>
                     </div>
